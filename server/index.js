8f36bcff9fae36fee15e55a7c4c897e188b0243e
“Paul <prowori2@gmail.com>
2019-10-05 18:13:35 +0300

    Setup babel

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 // import the app(routes)
-const http = require('http');
-const app = require('./server/app/app');
+const http = require("http");
+const app = require("./app/app");
 // import the database connection
-const db = require('./server/database/database');
+const db = require("./database/database");
 
 // create a server which hosts both
 
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 // Connect to the database(in database.js), then start the server with the app compiled from app.js
 db.connect().then(() => {
-  const server = http.createServer(app);
+    const server = http.createServer(app);
 
-  server.listen(port, () => console.log(`Server started on port ${port}`));
+    server.listen(port, () => console.log(`Server started on port ${port}`));
 });
