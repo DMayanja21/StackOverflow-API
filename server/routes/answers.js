c665d08d42549710c372f90fcb34a712215c9aac
Paul-Owori <prowori2@gmail.com>
2019-10-07 08:15:40 +0300

    Coveralls test

--- server/routes/answers.js
+++ server/routes/answers.js
@@ -5,8 +5,12 @@ const Answer = require("../models/Answer");
 
 // Token handling functions
 const tokenHandlers = require("./token-handling-functions");
-const { retrieveToken } = tokenHandlers;
-const { verifyToken } = tokenHandlers;
+const {
+    retrieveToken
+} = tokenHandlers;
+const {
+    verifyToken
+} = tokenHandlers;
 
 // All endpoints are JWT protected
 // Get a single answer object
@@ -16,7 +20,9 @@ router.get("/:answerID", retrieveToken, (req, res) => {
             // Authdata is available if required
             let answerID = req.params.answerID;
 
-            Answer.findOne({ _id: answerID })
+            Answer.findOne({
+                    _id: answerID
+                })
                 .then(result => {
                     if (result) {
                         res.status(200).json(result);
@@ -30,13 +36,21 @@ router.get("/:answerID", retrieveToken, (req, res) => {
                 .catch(err => {
                     let message = `An error occurred querying MongoDB for answer with ID:${answerID}`;
                     console.error(message, err);
-                    res.status(500).json({ status: 500, message, err });
+                    res.status(500).json({
+                        status: 500,
+                        message,
+                        err
+                    });
                 });
         })
         .catch(err => {
             let message = `An error occurred verifying token answer with ID:${answerID}`;
             console.error(message, err);
-            res.status(500).json({ status: 500, message, err });
+            res.status(500).json({
+                status: 500,
+                message,
+                err
+            });
         });
 });
 
@@ -44,6 +58,7 @@ router.get("/:answerID", retrieveToken, (req, res) => {
 router.get("/user/:userID", retrieveToken, (req, res) => {
     verifyToken(req.token).then(authData => {
         // Authdata is available if required
+
         let userID = req.params.userID;
         Answer.find({});
     });
