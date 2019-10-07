4c7648f1197cdc1fd5f870863d31beeb793326b5
Paul-Owori <prowori2@gmail.com>
2019-10-07 12:23:12 +0300

    Added questions skeleton router

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -6,9 +6,54 @@ const Question = require('../models/Question');
 // Token handling functions
 const tokenHandlers = require('./token-handling-functions');
 
-const { createToken } = tokenHandlers;
-const { retrieveToken } = tokenHandlers;
-const { verifyToken } = tokenHandlers;
+const {
+    retrieveToken
+} = tokenHandlers;
+const {
+    verifyToken
+} = tokenHandlers;
+
+// All endpoints are JWT protected
+// Get a single question object
+router.get("/:questionID", retrieveToken, (req, res) => {
+    verifyToken(req.token)
+        .then(authData => {
+            // Authdata is available if required
+            let questionID = req.params.questionID;
+
+            Question.findOne({
+                    _id: questionID
+                })
+                .then(result => {
+                    if (result) {
+                        res.status(200).json(result);
+                    } else {
+                        res.status(404).json({
+                            status: 404,
+                            message: "That question does not exist"
+                        });
+                    }
+                })
+                .catch(err => {
+                    let message = `An error occurred querying MongoDB for question with ID:${questionID}`;
+                    console.error(message, err);
+                    res.status(500).json({
+                        status: 500,
+                        message,
+                        err
+                    });
+                });
+        })
+        .catch(err => {
+            let message = `An error occurred verifying token question with ID:${questionID}`;
+            console.error(message, err);
+            res.status(500).json({
+                status: 500,
+                message,
+                err
+            });
+        });
+});
 
 
 module.exports = router;
