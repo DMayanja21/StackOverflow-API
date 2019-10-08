8938b3e70db801b40bcb68a23198c44eb6e77129
Paul-Owori <prowori2@gmail.com>
2019-10-08 08:50:30 +0300

    Cleaned commented out code from database file

--- server/database/database.js
+++ server/database/database.js
@@ -1,26 +1,10 @@
-const db = 'database';
-
 const mongoose = require('mongoose');
 
 const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://stackOverflowEDU:stackOverflowEDU@byarentcluster-gfhab.mongodb.net/stackOverflowEDU?retryWrites=true&w=majority';
 
-/**
- *
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://<username>:<password>@byarentcluster-gfhab.mongodb.net/admin?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
- */
-
-
+// Connect to database
 const connect = () => new Promise((resolve, reject) => {
   // Connect to the database then resolve the promise
-  // const conn = mongoose
   mongoose
     .connect(DB_URI, {
       useNewUrlParser: true,
@@ -38,6 +22,8 @@ const connect = () => new Promise((resolve, reject) => {
     });
 });
 
+
+// Switch off database
 const close = () => {
   console.log('Database gone offline');
   return mongoose.disconnect();
