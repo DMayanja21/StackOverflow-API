74fe8a3f6b840dce328c4c0a7bb659b659149700
“Paul <prowori2@gmail.com>
2019-10-09 18:41:05 +0300

    Testing one restructured test

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -1,227 +1,241 @@
 // Token handling functions
 import {
-  createToken,
-  retrieveToken,
-  verifyToken,
-} from './token-handling-functions';
+    createToken,
+    retrieveToken,
+    verifyToken
+} from "./token-handling-functions";
 
-
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const Question = require('../models/Question');
-const Answer = require('../models/Answer');
+const Question = require("../models/Question");
+const Answer = require("../models/Answer");
 
+// Get all questions
+// Unprotected endpoint
+router.get(
+    "/",
+    (req,
+    res => {
+        Question.find()
+            .then(results => {
+                if (results && results.length) {
+                    res.status(200).json(results);
+                } else {
+                    res.status(404).json({
+                        status: 404,
+                        message: "No questions found"
+                    });
+                }
+            })
+            .catch(err => {
+                const message = `An error occurred querying MongoDB for all questions`;
+                console.error(message, err);
+                res.status(500).json({
+                    status: 500,
+                    message,
+                    err
+                });
+            });
+    })
+);
 
 // Get a single question object
 // Unprotected endpoint
-router.get('/:questionID', (req, res) => {
-  const {
-    questionID,
-  } = req.params;
+router.get("/:questionID", (req, res) => {
+    const { questionID } = req.params;
 
-  Question.findOne({
-    _id: questionID,
-  })
-    .then((result) => {
-      if (result) {
-        res.status(200).json(result);
-      } else {
-        res.status(404).json({
-          status: 404,
-          message: 'That question does not exist',
-        });
-      }
+    Question.findOne({
+        _id: questionID
     })
-    .catch((err) => {
-      const message = `An error occurred querying MongoDB for question with ID:${questionID}`;
-      console.error(message, err);
-      res.status(500).json({
-        status: 500,
-        message,
-        err,
-      });
-    });
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    message: "That question does not exist"
+                });
+            }
+        })
+        .catch(err => {
+            const message = `An error occurred querying MongoDB for question with ID:${questionID}`;
+            console.error(message, err);
+            res.status(500).json({
+                status: 500,
+                message,
+                err
+            });
+        });
 });
 
 // Post a question
 // Endpoint protected by JWT
-router.post('/', retrieveToken, (req, res) => {
-  verifyToken(req.token)
-    .then((authData) => {
-      // authData is available if needed
-      if (authData === 403) {
-        res.status(403).json({
-          status: 403,
-          message: 'Invalid credentials',
-        });
-        return;
-      }
+router.post("/", retrieveToken, (req, res) => {
+    verifyToken(req.token)
+        .then(authData => {
+            // authData is available if needed
+            if (authData === 403) {
+                res.status(403).json({
+                    status: 403,
+                    message: "Invalid credentials"
+                });
+                return;
+            }
 
-      const {
-        user_id,
-      } = authData.user;
-      const {
-        title,
-      } = req.body;
-      const {
-        text,
-      } = req.body;
+            const { user_id } = authData.user;
+            const { title } = req.body;
+            const { text } = req.body;
 
-      const newQuestion = new Question({
-        user_id,
-        title,
-        text,
-      });
-
-      newQuestion.save()
-        .then((result) => {
-          res.status(201).json(
-            result,
-          );
-        }).catch((err) => {
-          if (err) {
-            const message = 'There was an error saving the question';
-            console.error(message, err);
-            res.status(500).json({
-              status: 500,
-              message,
-              err,
+            const newQuestion = new Question({
+                user_id,
+                title,
+                text
             });
-          }
+
+            newQuestion
+                .save()
+                .then(result => {
+                    res.status(201).json(result);
+                })
+                .catch(err => {
+                    if (err) {
+                        const message =
+                            "There was an error saving the question";
+                        console.error(message, err);
+                        res.status(500).json({
+                            status: 500,
+                            message,
+                            err
+                        });
+                    }
+                });
+            //
+        })
+        .catch(err => {
+            if (err) {
+                const message = "An error ocurred validating user token";
+                console.error(message, err);
+                res.status(500).json({
+                    status: 500,
+                    message,
+                    err
+                });
+            }
         });
-      //
-    })
-    .catch((err) => {
-      if (err) {
-        const message = 'An error ocurred validating user token';
-        console.error(message, err);
-        res.status(500).json({
-          status: 500,
-          message,
-          err,
-        });
-      }
-    });
 });
 
 // Get all questions by a user
-router.get('/user/:userID', (req, res) => {
-  const {
-    userID,
-  } = req.params;
+router.get("/user/:userID", (req, res) => {
+    const { userID } = req.params;
 
-  Question.find({
-    user_id: userID,
-  })
-    .then((result) => {
-      if (result) {
-        res.status(200).json(result);
-      } else {
-        res.status(404).json({
-          status: 404,
-          message: 'No questions found',
-        });
-      }
+    Question.find({
+        user_id: userID
     })
-    .catch((err) => {
-      if (err) {
-        const message = `No questions by user: ${userID} were found`;
-        console.error(message, err);
-        res.status(500).json({
-          message,
-          err,
-        });
-      }
-    });
-});
-
-
-// Delete a question
-router.delete('/:questionID', retrieveToken, (req, res) => {
-  verifyToken(req.token)
-    .then((authData) => {
-      if (authData === 403) {
-        res.status(403).json({
-          status: 403,
-          message: 'Invalid credentials',
-        });
-        return;
-      }
-
-
-      const {
-        questionID,
-      } = req.params;
-
-      // Create the object that will be returned
-      const resultsOfOperation = {};
-
-      Question.findByIdAndDelete({
-        _id: questionID,
-      })
-        .then((result) => {
-          if (result !== null) {
-            // Attach results of deleting qn to the resultsOfOperation object
-            resultsOfOperation.qnResult = result;
-            Answer.deleteMany({
-              question_id: questionID,
-            })
-              .then((result) => {
-                // If result === null or result.n===0, then no answers were found
-                if (result !== null && result.n !== 0) {
-                  resultsOfOperation.ansResults = result;
-
-                  return resultsOfOperation;
-                }
-                resultsOfOperation.ansResults = {
-                  message: 'No answers were attached to the qn',
-                };
-                return resultsOfOperation;
-              })
-              .then((results) => {
-                res.status(200).json(results);
-              })
-              .catch((err) => {
-                const message = `Error deleting answers related to question:${questionID}`;
+        .then(result => {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    message: "No questions found"
+                });
+            }
+        })
+        .catch(err => {
+            if (err) {
+                const message = `No questions by user: ${userID} were found`;
                 console.error(message, err);
                 res.status(500).json({
-                  message,
-                  err,
+                    message,
+                    err
                 });
-              });
-          } else {
-            const message = `Error 404 deleting question:${questionID}, Not found`;
-            console.error(message);
-            res.status(404).json({
-              message,
+            }
+        });
+});
 
-            });
-          }
+// Delete a question
+router.delete("/:questionID", retrieveToken, (req, res) => {
+    verifyToken(req.token)
+        .then(authData => {
+            if (authData === 403) {
+                res.status(403).json({
+                    status: 403,
+                    message: "Invalid credentials"
+                });
+                return;
+            }
 
-          // return resultsOfOperation
+            const { questionID } = req.params;
+
+            // Create the object that will be returned
+            const resultsOfOperation = {};
+
+            Question.findByIdAndDelete({
+                _id: questionID
+            })
+                .then(result => {
+                    if (result !== null) {
+                        // Attach results of deleting qn to the resultsOfOperation object
+                        resultsOfOperation.qnResult = result;
+                        Answer.deleteMany({
+                            question_id: questionID
+                        })
+                            .then(result => {
+                                // If result === null or result.n===0, then no answers were found
+                                if (result !== null && result.n !== 0) {
+                                    resultsOfOperation.ansResults = result;
+
+                                    return resultsOfOperation;
+                                }
+                                resultsOfOperation.ansResults = {
+                                    message:
+                                        "No answers were attached to the qn"
+                                };
+                                return resultsOfOperation;
+                            })
+                            .then(results => {
+                                res.status(200).json(results);
+                            })
+                            .catch(err => {
+                                const message = `Error deleting answers related to question:${questionID}`;
+                                console.error(message, err);
+                                res.status(500).json({
+                                    message,
+                                    err
+                                });
+                            });
+                    } else {
+                        const message = `Error 404 deleting question:${questionID}, Not found`;
+                        console.error(message);
+                        res.status(404).json({
+                            message
+                        });
+                    }
+
+                    // return resultsOfOperation
+                })
+                .catch(err => {
+                    if (err) {
+                        const message = `Error deleting question:${questionID}`;
+                        console.error(message, err);
+                        res.status(500).json({
+                            message,
+                            err
+                        });
+                    }
+                });
         })
-        .catch((err) => {
-          if (err) {
-            const message = `Error deleting question:${questionID}`;
-            console.error(message, err);
-            res.status(500).json({
-              message,
-              err,
-            });
-          }
+        .catch(err => {
+            if (err) {
+                const message = `Error verifying user token to delete question:${questionID}`;
+                console.error(message, err);
+                res.status(500).json({
+                    message,
+                    err
+                });
+            }
         });
-    })
-    .catch((err) => {
-      if (err) {
-        const message = `Error verifying user token to delete question:${questionID}`;
-        console.error(message, err);
-        res.status(500).json({
-          message,
-          err,
-        });
-      }
-    });
 });
 
 module.exports = router;
--- test/server/routes/questions-test.js
+++ test/server/routes/questions-test.js
@@ -1,39 +1,38 @@
 // Call testing languages
-import chai from 'chai';
-import chaiHttp from 'chai-http';
+import chai from "chai";
+import chaiHttp from "chai-http";
 
 // Call mongoose and memory-server to create a fake db for testing purposes
-import mongoose from 'mongoose';
-import MongoMemoryServer from 'mongodb-memory-server-core';
+import mongoose from "mongoose";
+import MongoMemoryServer from "mongodb-memory-server-core";
 
 // Import the app we're testing
-import app from '../../../server/app/app';
+import app from "../../../server/app/app";
 
 chai.use(chaiHttp);
-const {
-  expect,
-  request,
-} = chai;
+const { expect, request } = chai;
 let mongoServer;
 
-before((done) => {
-  mongoServer = new MongoMemoryServer();
-  const opts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  };
-  mongoServer
-    .getConnectionString()
-    .then((mongoUri) => mongoose.connect(mongoUri, opts, (err) => {
-      if (err) done(err);
-    }))
-    .then(() => done());
+before(done => {
+    mongoServer = new MongoMemoryServer();
+    const opts = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    };
+    mongoServer
+        .getConnectionString()
+        .then(mongoUri =>
+            mongoose.connect(mongoUri, opts, err => {
+                if (err) done(err);
+            })
+        )
+        .then(() => done());
 });
 
 after(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+    await mongoose.disconnect();
+    await mongoServer.stop();
 });
 
 // The token that will be used for JWT auth
@@ -45,148 +44,146 @@ let questionID;
 
 // Function containing all the tests for the questions router
 // Logs the purpose of the tests to the console
-describe('Test all API endpoints for /questions', () => {
-  // Test 1: test user signup
-  it('First creates a new user via /auth because several /questions endpoints are JWT protected', (done) => {
+describe("Creates a new user for testing /questions ", () => {
+    // Test 1: test user signup
+    let status;
     request(app)
-      .post('/auth/signup')
-      .send({
-        firstName: 'Bob',
-        lastName: 'Paul',
-        emailAddress: 'email@address.com',
-        password: '123',
-      })
-      .then((res) => {
-        const {
-          status,
-        } = res;
+        .post("/auth/signup")
+        .send({
+            firstName: "Bob",
+            lastName: "Paul",
+            emailAddress: "email@address.com",
+            password: "123"
+        })
+        .then(res => {
+            status = res.status;
+        })
+        .catch(err => {
+            console.log(
+                `An error occurred testing /auth/signup endpoint Error:${err}`
+            );
+        });
+
+    it("Checks if the server response has a status of 201", done => {
         expect(status).to.equal(201);
         done();
-      })
-      .catch((err) => {
-        console.log(
-          `An error occurred testing /auth/signup endpoint Error:${err}`,
-        );
-      });
-  });
+    });
 
+    // it('Logs in newly created user with the correct credentials', (done) => {
+    //   request(app)
+    //     .post('/auth/login')
+    //     .send({
+    //       emailAddress: 'email@address.com',
+    //       password: '123',
+    //     })
+    //     .then((res) => {
+    //       const {
+    //         status,
+    //       } = res;
+    //       const {
+    //         token,
+    //       } = res.body;
+    //       testToken = token;
+    //       expect(status).to.equal(200);
+    //       expect(token).to.exist;
+    //       expect(typeof token === 'string').to.equal(true);
+    //       done();
+    //     })
+    //     .catch((err) => {
+    //       console.log(
+    //         `An error occurred testing /auth/login endpoint Error:${err}`,
+    //       );
+    //     });
+    // });
 
-  it('Logs in newly created user with the correct credentials', (done) => {
-    request(app)
-      .post('/auth/login')
-      .send({
-        emailAddress: 'email@address.com',
-        password: '123',
-      })
-      .then((res) => {
-        const {
-          status,
-        } = res;
-        const {
-          token,
-        } = res.body;
-        testToken = token;
-        expect(status).to.equal(200);
-        expect(token).to.exist;
-        expect(typeof token === 'string').to.equal(true);
-        done();
-      })
-      .catch((err) => {
-        console.log(
-          `An error occurred testing /auth/login endpoint Error:${err}`,
-        );
-      });
-  });
+    // it('Asks / Posts a question', (done) => {
+    //   request(app)
+    //     .post('/questions')
+    //     .set('Authorization', `Bearer ${testToken}`)
+    //     .send({
+    //       title: 'Question title',
+    //       text: 'Question text',
+    //     })
+    //     .then((res) => {
+    //       const {
+    //         status,
+    //       } = res;
+    //       const response = res.body;
+    //       // Set the user id which will be used in later tests
+    //       userID = response.user_id;
+    //       questionID = response._id;
 
+    //       // Conditions to test
+    //       expect(status).to.equal(201);
+    //       expect(response).to.be.an('object');
+    //       expect(response).to.include.all.keys('status', '_id', 'user_id', 'title', 'text');
+    //       done();
+    //     })
+    //     .catch((err) => {
+    //       const message = 'An error occurred testing POST /questions endpoint';
+    //       console.error(message, err);
+    //     });
+    // });
 
-  it('Asks / Posts a question', (done) => {
-    request(app)
-      .post('/questions')
-      .set('Authorization', `Bearer ${testToken}`)
-      .send({
-        title: 'Question title',
-        text: 'Question text',
-      })
-      .then((res) => {
-        const {
-          status,
-        } = res;
-        const response = res.body;
-        // Set the user id which will be used in later tests
-        userID = response.user_id;
-        questionID = response._id;
+    // // Unprotected endpoint
+    // it('Gets a single question', (done) => {
+    //   request(app)
+    //     .get(`/questions/${questionID}`)
+    //     .then((res) => {
+    //       const {
+    //         status,
+    //       } = res;
+    //       const response = res.body;
 
-        // Conditions to test
-        expect(status).to.equal(201);
-        expect(response).to.be.an('object');
-        expect(response).to.include.all.keys('status', '_id', 'user_id', 'title', 'text');
-        done();
-      })
-      .catch((err) => {
-        const message = 'An error occurred testing POST /questions endpoint';
-        console.error(message, err);
-      });
-  });
+    //       // Conditions to test
+    //       expect(status).to.equal(200);
+    //       expect(response).to.be.an('object');
+    //       expect(response).to.include.all.keys('status', '_id', 'user_id', 'title', 'text');
+    //       done();
+    //     })
+    //     .catch((err) => {
+    //       const message = 'An error occurred testing POST /questions endpoint';
+    //       console.error(message, err);
+    //     });
+    // });
 
-  // Unprotected endpoint
-  it('Gets a single question', (done) => {
-    request(app)
-      .get(`/questions/${questionID}`)
-      .then((res) => {
-        const {
-          status,
-        } = res;
-        const response = res.body;
+    // it('Gets all questions by a user', (done) => {
+    //   request(app)
+    //     .get(`/questions/user/${userID}`)
+    //     .set('Authorization', `Bearer ${testToken}`)
+    //     .then((res) => {
+    //       const {
+    //         status,
+    //       } = res;
+    //       const response = res.body;
 
-        // Conditions to test
-        expect(status).to.equal(200);
-        expect(response).to.be.an('object');
-        expect(response).to.include.all.keys('status', '_id', 'user_id', 'title', 'text');
-        done();
-      })
-      .catch((err) => {
-        const message = 'An error occurred testing POST /questions endpoint';
-        console.error(message, err);
-      });
-  });
+    //       expect(status).to.equal(200);
+    //       expect(response).to.exist;
+    //       expect(response).to.be.an('array');
+    //       done();
+    //     })
+    //     .catch((err) => {
+    //       const message = 'An error occurred testing GET /questions/:userID endpoint';
+    //       console.error(message, err);
+    //     });
+    // });
 
-  it('Gets all questions by a user', (done) => {
-    request(app)
-      .get(`/questions/user/${userID}`)
-      .set('Authorization', `Bearer ${testToken}`)
-      .then((res) => {
-        const {
-          status,
-        } = res;
-        const response = res.body;
+    // it('Deletes a question', (done) => {
+    //   request(app)
+    //     .delete(`/questions/${questionID}`)
+    //     .set('Authorization', `Bearer ${testToken}`)
+    //     .then((res) => {
+    //       const {
+    //         status,
+    //       } = res;
+    //       const response = res.body;
 
-        expect(status).to.equal(200);
-        expect(response).to.exist;
-        expect(response).to.be.an('array');
-        done();
-      })
-      .catch((err) => {
-        const message = 'An error occurred testing GET /questions/:userID endpoint';
-        console.error(message, err);
-      });
-  });
-
-  it('Deletes a question', (done) => {
-    request(app)
-      .delete(`/questions/${questionID}`)
-      .set('Authorization', `Bearer ${testToken}`)
-      .then((res) => {
-        const {
-          status,
-        } = res;
-        const response = res.body;
-
-        // Conditions to test
-        expect(status).to.equal(200);
-        expect(response).to.exist;
-        expect(response).to.be.an('object');
-        expect(response).to.include.all.keys('qnResult', 'ansResults');
-        done();
-      });
-  });
+    //       // Conditions to test
+    //       expect(status).to.equal(200);
+    //       expect(response).to.exist;
+    //       expect(response).to.be.an('object');
+    //       expect(response).to.include.all.keys('qnResult', 'ansResults');
+    //       done();
+    //     });
+    // });
 });
