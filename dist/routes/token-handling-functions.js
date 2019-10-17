535282ef7cbf7edfb48af9fe55aa21716eb53770
“Paul <prowori2@gmail.com>
2019-10-17 06:45:25 +0300

    Fixed broken require module

---
+++ dist/routes/token-handling-functions.js
@@ -0,0 +1,562 @@
+"use strict";
+
+var cov_1b1lrt4p1y = function () {
+  var path = "/Users/Paul/Desktop/Repositories/Github/StackOverflow-API/server/routes/token-handling-functions.js";
+  var hash = "46e04093f0624a0f9a33a95ca84787bec00c8c73";
+  var global = new Function("return this")();
+  var gcv = "__coverage__";
+  var coverageData = {
+    path: "/Users/Paul/Desktop/Repositories/Github/StackOverflow-API/server/routes/token-handling-functions.js",
+    statementMap: {
+      "0": {
+        start: {
+          line: 3,
+          column: 23
+        },
+        end: {
+          line: 3,
+          column: 41
+        }
+      },
+      "1": {
+        start: {
+          line: 9,
+          column: 2
+        },
+        end: {
+          line: 13,
+          column: 43
+        }
+      },
+      "2": {
+        start: {
+          line: 13,
+          column: 19
+        },
+        end: {
+          line: 13,
+          column: 40
+        }
+      },
+      "3": {
+        start: {
+          line: 15,
+          column: 2
+        },
+        end: {
+          line: 15,
+          column: 23
+        }
+      },
+      "4": {
+        start: {
+          line: 21,
+          column: 23
+        },
+        end: {
+          line: 21,
+          column: 48
+        }
+      },
+      "5": {
+        start: {
+          line: 23,
+          column: 2
+        },
+        end: {
+          line: 36,
+          column: 3
+        }
+      },
+      "6": {
+        start: {
+          line: 25,
+          column: 19
+        },
+        end: {
+          line: 25,
+          column: 42
+        }
+      },
+      "7": {
+        start: {
+          line: 26,
+          column: 24
+        },
+        end: {
+          line: 26,
+          column: 33
+        }
+      },
+      "8": {
+        start: {
+          line: 29,
+          column: 4
+        },
+        end: {
+          line: 29,
+          column: 28
+        }
+      },
+      "9": {
+        start: {
+          line: 30,
+          column: 4
+        },
+        end: {
+          line: 30,
+          column: 11
+        }
+      },
+      "10": {
+        start: {
+          line: 32,
+          column: 4
+        },
+        end: {
+          line: 35,
+          column: 7
+        }
+      },
+      "11": {
+        start: {
+          line: 40,
+          column: 19
+        },
+        end: {
+          line: 48,
+          column: 4
+        }
+      },
+      "12": {
+        start: {
+          line: 41,
+          column: 4
+        },
+        end: {
+          line: 45,
+          column: 5
+        }
+      },
+      "13": {
+        start: {
+          line: 42,
+          column: 6
+        },
+        end: {
+          line: 42,
+          column: 41
+        }
+      },
+      "14": {
+        start: {
+          line: 43,
+          column: 21
+        },
+        end: {
+          line: 43,
+          column: 24
+        }
+      },
+      "15": {
+        start: {
+          line: 44,
+          column: 6
+        },
+        end: {
+          line: 44,
+          column: 20
+        }
+      },
+      "16": {
+        start: {
+          line: 47,
+          column: 4
+        },
+        end: {
+          line: 47,
+          column: 20
+        }
+      },
+      "17": {
+        start: {
+          line: 50,
+          column: 2
+        },
+        end: {
+          line: 50,
+          column: 18
+        }
+      }
+    },
+    fnMap: {
+      "0": {
+        name: "createToken",
+        decl: {
+          start: {
+            line: 6,
+            column: 22
+          },
+          end: {
+            line: 6,
+            column: 33
+          }
+        },
+        loc: {
+          start: {
+            line: 6,
+            column: 40
+          },
+          end: {
+            line: 16,
+            column: 1
+          }
+        },
+        line: 6
+      },
+      "1": {
+        name: "(anonymous_1)",
+        decl: {
+          start: {
+            line: 13,
+            column: 2
+          },
+          end: {
+            line: 13,
+            column: 3
+          }
+        },
+        loc: {
+          start: {
+            line: 13,
+            column: 19
+          },
+          end: {
+            line: 13,
+            column: 40
+          }
+        },
+        line: 13
+      },
+      "2": {
+        name: "retrieveToken",
+        decl: {
+          start: {
+            line: 19,
+            column: 16
+          },
+          end: {
+            line: 19,
+            column: 29
+          }
+        },
+        loc: {
+          start: {
+            line: 19,
+            column: 46
+          },
+          end: {
+            line: 37,
+            column: 1
+          }
+        },
+        line: 19
+      },
+      "3": {
+        name: "verifyToken",
+        decl: {
+          start: {
+            line: 39,
+            column: 22
+          },
+          end: {
+            line: 39,
+            column: 33
+          }
+        },
+        loc: {
+          start: {
+            line: 39,
+            column: 41
+          },
+          end: {
+            line: 51,
+            column: 1
+          }
+        },
+        line: 39
+      },
+      "4": {
+        name: "(anonymous_4)",
+        decl: {
+          start: {
+            line: 40,
+            column: 64
+          },
+          end: {
+            line: 40,
+            column: 65
+          }
+        },
+        loc: {
+          start: {
+            line: 40,
+            column: 83
+          },
+          end: {
+            line: 48,
+            column: 3
+          }
+        },
+        line: 40
+      }
+    },
+    branchMap: {
+      "0": {
+        loc: {
+          start: {
+            line: 23,
+            column: 2
+          },
+          end: {
+            line: 36,
+            column: 3
+          }
+        },
+        type: "if",
+        locations: [{
+          start: {
+            line: 23,
+            column: 2
+          },
+          end: {
+            line: 36,
+            column: 3
+          }
+        }, {
+          start: {
+            line: 23,
+            column: 2
+          },
+          end: {
+            line: 36,
+            column: 3
+          }
+        }],
+        line: 23
+      },
+      "1": {
+        loc: {
+          start: {
+            line: 41,
+            column: 4
+          },
+          end: {
+            line: 45,
+            column: 5
+          }
+        },
+        type: "if",
+        locations: [{
+          start: {
+            line: 41,
+            column: 4
+          },
+          end: {
+            line: 45,
+            column: 5
+          }
+        }, {
+          start: {
+            line: 41,
+            column: 4
+          },
+          end: {
+            line: 45,
+            column: 5
+          }
+        }],
+        line: 41
+      }
+    },
+    s: {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+      "6": 0,
+      "7": 0,
+      "8": 0,
+      "9": 0,
+      "10": 0,
+      "11": 0,
+      "12": 0,
+      "13": 0,
+      "14": 0,
+      "15": 0,
+      "16": 0,
+      "17": 0
+    },
+    f: {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0
+    },
+    b: {
+      "0": [0, 0],
+      "1": [0, 0]
+    },
+    _coverageSchema: "43e27e138ebf9cfc5966b082cf9a028302ed4184",
+    hash: "46e04093f0624a0f9a33a95ca84787bec00c8c73"
+  };
+  var coverage = global[gcv] || (global[gcv] = {});
+
+  if (coverage[path] && coverage[path].hash === hash) {
+    return coverage[path];
+  }
+
+  return coverage[path] = coverageData;
+}();
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.createToken = createToken;
+exports.retrieveToken = retrieveToken;
+exports.verifyToken = verifyToken;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+var tokenSecretKey = (cov_1b1lrt4p1y.s[0]++, 'stackOverflowAPI'); // Function to create token on login
+
+function createToken(_x) {
+  return _createToken.apply(this, arguments);
+} // MIDDLEWARE function to retrieve token from request headers
+
+
+function _createToken() {
+  _createToken = (0, _asyncToGenerator2["default"])(
+  /*#__PURE__*/
+  _regenerator["default"].mark(function _callee(user) {
+    var tokenToReturn;
+    return _regenerator["default"].wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            cov_1b1lrt4p1y.f[0]++;
+            cov_1b1lrt4p1y.s[1]++;
+            _context.next = 4;
+            return _jsonwebtoken["default"].sign({
+              user: user
+            }, "".concat(tokenSecretKey), function (err, token) {
+              cov_1b1lrt4p1y.f[1]++;
+              cov_1b1lrt4p1y.s[2]++;
+              return tokenToReturn = token;
+            });
+
+          case 4:
+            cov_1b1lrt4p1y.s[3]++;
+            return _context.abrupt("return", tokenToReturn);
+
+          case 6:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+  return _createToken.apply(this, arguments);
+}
+
+function retrieveToken(req, res, next) {
+  cov_1b1lrt4p1y.f[2]++;
+  // Get authorization header value
+  var bearerHeader = (cov_1b1lrt4p1y.s[4]++, req.headers.authorization); // Check if bearerHeader is undefned
+
+  cov_1b1lrt4p1y.s[5]++;
+
+  if (bearerHeader !== undefined) {
+    cov_1b1lrt4p1y.b[0][0]++;
+    // Get the token out of the bearer.
+    var bearer = (cov_1b1lrt4p1y.s[6]++, bearerHeader.split(' '));
+    var bearerToken = (cov_1b1lrt4p1y.s[7]++, bearer[1]); // Set the token
+
+    cov_1b1lrt4p1y.s[8]++;
+    req.token = bearerToken;
+    cov_1b1lrt4p1y.s[9]++;
+    next();
+  } else {
+    cov_1b1lrt4p1y.b[0][1]++;
+    cov_1b1lrt4p1y.s[10]++;
+    res.status(403).json({
+      status: 403,
+      error: 'No token found.'
+    });
+  }
+}
+
+function verifyToken(_x2) {
+  return _verifyToken.apply(this, arguments);
+} // module.exports = {
+//     createToken,
+//     retrieveToken,
+//     verifyToken
+// };
+
+
+function _verifyToken() {
+  _verifyToken = (0, _asyncToGenerator2["default"])(
+  /*#__PURE__*/
+  _regenerator["default"].mark(function _callee2(token) {
+    var authInfo;
+    return _regenerator["default"].wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            cov_1b1lrt4p1y.f[3]++;
+            cov_1b1lrt4p1y.s[11]++;
+            _context2.next = 4;
+            return _jsonwebtoken["default"].verify(token, "".concat(tokenSecretKey), function (err, authData) {
+              cov_1b1lrt4p1y.f[4]++;
+              cov_1b1lrt4p1y.s[12]++;
+
+              if (err) {
+                cov_1b1lrt4p1y.b[1][0]++;
+                cov_1b1lrt4p1y.s[13]++;
+                console.log('Invalid credentials');
+                var status = (cov_1b1lrt4p1y.s[14]++, 403);
+                cov_1b1lrt4p1y.s[15]++;
+                return status;
+              } else {
+                cov_1b1lrt4p1y.b[1][1]++;
+              }
+
+              cov_1b1lrt4p1y.s[16]++;
+              return authData;
+            });
+
+          case 4:
+            authInfo = _context2.sent;
+            cov_1b1lrt4p1y.s[17]++;
+            return _context2.abrupt("return", authInfo);
+
+          case 7:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+  return _verifyToken.apply(this, arguments);
+}
+//# sourceMappingURL=token-handling-functions.js.map
